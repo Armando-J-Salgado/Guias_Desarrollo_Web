24d7d7a0a1581114fed8f33451cb0073d0f8bd84
Armando Salgado <20245138@esen.edu.sv>
2025-11-01 17:23:33 -0600

    Inicio de la guía 6

---
+++ Guia6_20245138/js/registroAcademico.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", function() {
+    //Accedemos al contenedor donde se mostrara los estudiantes
+    const containerEstudiantes = document.querySelector(
+        "#idContainerEstudiantes"
+    );
+
+    //Accedemos a cada boton por medio de la API DOM
+    const btnAddEstudiante = document.querySelector("#idBtnAgregarEstudiante");
+    const btnViewEstudiantes = document.querySelector("#idBtnMostrarEstudiantes");
+})
